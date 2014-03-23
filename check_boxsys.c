abd4b76e60c8667108596f4dc0059f433581854a
Alex AUVOLAT <alex.auvolat@ens.fr>
2014-03-23 19:26:34 +0100

    Added C checker and some data.

---
+++ check_boxsys.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+
+
+/* Utility */
+
+void print_intlist(char* p, int n, int* perm) {
+	int i;
+	printf("%s: [", p);
+	for(i = 0; i < n; i++) {
+		if (i != 0) printf(", ");
+		printf("%d", perm[i]);
+	}
+	printf("]\n");
+}
+
+long long int fact(long long int n) {
+	if (n == 0)
+		return 1;
+	else
+		return n * fact(n-1);
+}
+
+/* Permutations */
+
+typedef long long int id;
+
+id id_of_perm(const int n, int* perm) {
+	int i, j;
+	int lehmer[n];
+	id res = 0;
+
+	for (i = 0; i < n; i++) lehmer[i] = perm[i];
+
+	for (i = 0; i < n; i++) {
+		res += lehmer[i];
+		if (i != n-1) res *= n - 1 - i;
+
+		for (j = i + 1; j < n; j++) {
+			if (lehmer[j] > lehmer[i]) lehmer[j]--;
+		}
+	}
+	// print_intlist("Lehmer (id_of_perm)", n, lehmer);
+
+	return res;
+}
+
+void perm_of_id(id x, const int n, int* perm) {
+	int i, j, t;
+	int lehmer[n];
+
+	for (i = n-1; i >= 0; i--) {
+		lehmer[i] = x % (n-i);
+		x /= (n-i);
+	}
+
+	// print_intlist("Lehmer (perm_of_id)", n, lehmer);
+
+	for (i = 0; i < n; i++) perm[i] = i;
+
+	for (i = 0; i < n; i++) {
+		t = perm[i + lehmer[i]];
+		for (j = i + lehmer[i]; j > i; j--) perm[j] = perm[j-1];
+		perm[i] = t;
+	}
+}
+
+/* Bitsets */
+
+int bitset_get(unsigned char* bitset, id x) {
+	id pos = x / 8;
+	id sh = x % 8;
+	return (bitset[pos] >> sh) & 1;
+}
+
+int bitset_set(unsigned char* bitset, id x) {
+	id pos = x / 8;
+	id sh = x % 8;
+	bitset[pos] |= (1 << sh);
+}
+
+int bitset_unset(unsigned char* bitset, id x) {
+	id pos = x / 8;
+	id sh = x % 8;
+	bitset[pos] &= ~(1 << sh);
+}
+
+/* Permset checking */
+
+/*		n = nombre de fils
+		p = nombre de boites
+		l = fil gauche de la boite #i
+		r = fil droite de la boite #i
+*/
+
+int check_permset(const int n, const int p, const int* l, const int* r) {
+	id x, y;
+	int i, t;
+
+	id perms = fact(n);
+	unsigned char T[perms/8+1];
+	for (x = 0; x < perms/8+1; x++) T[x] = 0;
+	bitset_set(T, 0);
+
+	for (i = 0; i < p; i++) {
+		for (x = 0; x < perms; x++) {
+			if (bitset_get(T, x)) {
+				int perm[n];
+				perm_of_id(x, n, perm);
+				t = perm[l[i]];
+				perm[l[i]] = perm[r[i]];
+				perm[r[i]] = t;
+				bitset_set(T, id_of_perm(n, perm));
+			}
+		}
+	}
+
+	for (x = 0; x < perms; x++) {
+		if (!bitset_get(T, x)) return 0;
+	}
+	return 1;
+}
+
+
+int main() {
+	int i;
+	int n, p;
+
+	scanf(" %d %d", &n, &p);
+	int l[p], r[p];
+
+	fprintf(stderr, "Checking permset n=%d p=%d\n", n, p);
+	
+	for(i = 0; i < p; i++) {
+		scanf(" %d %d", &l[i], &r[i]);
+	}
+	
+	printf ("%d\n", check_permset(n, p, l, r));
+
+	return 0;
+}
